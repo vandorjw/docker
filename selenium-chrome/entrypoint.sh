95b18b5f8cc51095c505dcf0bf4f6767f9cdf81a
Joost VanDorp <joostvandorp@gmail.com>
2018-03-08 17:07:38 -0500

    RetailMeNotSandbox/ecs-selenium

--- selenium-chrome/entrypoint.sh
+++ selenium-chrome/entrypoint.sh
@@ -7,5 +7,11 @@ set -o nounset
 : "${lb_dns:="none"}"
 HUB_HOST="$lb_dns"
 
+result="$(python /opt/bin/ecs-get-port-mapping.py)"
+eval "$result"
+
+export SE_OPTS="-host $EC2_HOST -remoteHost http://$EC2_HOST:$PORT_TCP_5555 -id $NODE_ID"
+
+# execute default extry_point.sh file
 /opt/bin/entry_point.sh
 
